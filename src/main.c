20ba76ee3a5a985cfce4a719192e8eefe10949be
Vai Viswanathan <vviswanathan@hmc.edu>
2014-08-12 12:36:00 -0500

    Revert "Quick Tap integration"
    This reverts commit 282b8127017a199bde735f300c113a5ba5a5daf7.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@ Trek font taken from PebbleTrek by Kyle Potts: https://github.com/kylepotts/pebb
 */
 
 #include <pebble.h>
-#include "QTPlus.h"
 #define NUM_ANIMATION_PKEY 1
 #define NUM_ANIMATION_DEFAULT 1
 
@@ -397,7 +396,6 @@ void deinit()
   //Write the last animation to storage
   persist_write_int(NUM_ANIMATION_PKEY, num_animation);
   animation_unschedule_all();
-  qtp_app_deinit();
   tick_timer_service_unsubscribe();
  	battery_state_service_unsubscribe();
   bluetooth_connection_service_unsubscribe();
@@ -406,9 +404,7 @@ void deinit()
  
 int main(void)
 {
-	qtp_conf = QTP_K_SHOW_TIME | QTP_K_AUTOHIDE | QTP_K_INVERT;
 	init();
-	qtp_setup();
 	app_event_loop();
   deinit();
 }
